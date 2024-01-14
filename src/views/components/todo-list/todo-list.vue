589d6beb334fe5ee5004686e2b2e419ef30770db
Tawing <yabuking84@gmail.com>
2024-01-14 08:40:12 +0400

    margin

--- src/views/components/todo-list/todo-list.vue
+++ src/views/components/todo-list/todo-list.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container mx-auto w-full">
-    <div class="max-w-xl sm:min-w-96 mx-2 sm:mx-auto bg-white shadow-lg rounded-lg overflow-hidden my-16 py-4">
+    <div class="max-w-xl sm:min-w-96 m-2 sm:mx-auto bg-white shadow-lg rounded-lg overflow-hidden md:my-8 py-4">
       <div class="flex justify-between px-4 py-2">
         <h1 class="text-gray-800 font-bold text-2xl uppercase">To-Do List</h1>
 
