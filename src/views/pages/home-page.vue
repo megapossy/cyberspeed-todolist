58eb3ab9e10db9755226109208ff7680fbb63a2c
Tawing <yabuking84@gmail.com>
2024-01-15 07:24:29 +0400

    transition

--- src/views/pages/home-page.vue
+++ src/views/pages/home-page.vue
@@ -1,13 +1,25 @@
 <template>
   <main class="w-full">
     <div class="container mx-auto w-full p-2 sm:pt-20 flex justify-center items-start">
-      <TodoList class="w-full" />
+      <FadeUp>
+        <TodoList v-show="isShown" class="w-full" />
+      </FadeUp>
     </div>
   </main>
 </template>
 
 <script setup lang="ts">
+import helpers from "@UTILS/helpers";
 import TodoList from "@CMPNTS/todo-list/todo-list.vue"
+import { onMounted, ref } from "vue";
+
+const isShown = ref(false)
+
+onMounted(async () => {
+  await helpers.huwaits(500)
+  isShown.value = true
+})
+
 </script>
 
 <style scoped></style>
